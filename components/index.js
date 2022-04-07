fb9218e593719175505d0377e593ccad98a2daf2
kxngj <jaheimcbrown@gmail.com>
2022-04-07 13:19:14 -0500

    Added Details Screen Components

--- components/index.js
+++ components/index.js
@@ -2,5 +2,17 @@ import { default as HomeHeader } from "./HomeHeader";
 import { default as FocusedStatusBar } from "./FocusedStatusBar";
 import { default as NFTCard } from "./NFTCard";
 import { RectButton, CircleButton } from "./Button";
+import { default as DetailsDesc } from "./DetailsDesc";
+import { default as DetailsBid } from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
 
-export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectButton };
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailsDesc,
+  DetailsBid,
+  SubInfo,
+};
